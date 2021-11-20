d5cdebf9f3119175dae15b7616d2978d28623588
AhmedRedaMahmoud <94741365+AhmedRedaMahmoud@users.noreply.github.com>
2021-11-20 13:13:42 +0200

    Update HIVE-HQL.sql

--- HIVE-HQL.sql
+++ HIVE-HQL.sql
@@ -2,6 +2,6 @@ use default;
 
 DESCRIBE employee;
 
-insert into employee (eid,	name	,salary	,destination	) values(8,"Ahmed Reda",1500,"Training");
+insert into employee (eid,name,salary,destination) values(8,"Ahmed Reda",1500,"Training");
 
 select * from employee where eid = 8;
